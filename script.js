6b22433ea6f505cbdbd79be5668398337f008b8d
Rose <rose1773@stud.kea.dk>
2022-06-11 18:12:05 +0200

    svg, ny bagr farve og start på js

---
+++ script.js
@@ -0,0 +1,4 @@
+window.addEventListener("load", sidenVises);
+//Start op_ned-animationer
+document.querySelector("#hjerte1_container").classList.add("op_ned");
+document.querySelector("#hjerte2_container").classList.add("op_ned");
