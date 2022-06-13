aa9474fdf45a4f05ee86b4e72526f09a80f1eb9d
Rose <rose1773@stud.kea.dk>
2022-06-13 14:37:30 +0200

    javascript

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
 window.addEventListener("load", sidenVises);
 //Start op_ned-animationer
-document.querySelector("#hjerte1_container").classList.add("op_ned");
-document.querySelector("#hjerte2_container").classList.add("op_ned");
+document.querySelector("#portfolio_container").classList.add("op_ned");
+document.querySelector("#navn_container").classList.add("op_ned");
+
+function sidenVises() {
+  console.log("sidenVises");
+}
+//Forsvind animationer
+document.querySelector("#hjerte1_sprite").classList.add("forsvind");
+document.querySelector("#hjerte2_sprite").classList.add("forsvind");
+document.querySelector("#hjerte3_sprite").classList.add("forsvind");
